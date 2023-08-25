714f24a21dd4cf19a34ce23744620322f98cf3e4
Alina <137694649+BukovskaA@users.noreply.github.com>
2023-08-25 14:56:04 +0300

    Delete task1.sql

--- sql/task1.sql
+++
@@ -1,8 +0,0 @@
-select username, id 
-from users
-inner join (select guest_id, count(guest_id) as reservations_count
-from reservations
-group by guest_id
-order by (reservations_count) desc
-limit 1) as filtered
-on users.id = filtered.guest_id
